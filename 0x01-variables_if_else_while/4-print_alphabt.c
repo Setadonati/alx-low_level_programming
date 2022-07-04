764c33f4c64fb38f0fd6505397a054ad3ed2aa6a
Setadonati <setadonati@gmail.com>
2022-07-04 08:53:15 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,8 @@ char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' && letter !='q') putchar(letter);
+if (letter != 'e' && letter != 'q')
+putchar(letter);
 }
 
 putchar('\n');
