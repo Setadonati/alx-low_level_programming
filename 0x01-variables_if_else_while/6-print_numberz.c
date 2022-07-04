d9868312eb8945b3a1df1d8ff03ee4fbd29391fa
Setadonati <setadonati@gmail.com>
2022-07-04 13:05:14 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - Prints all single digit numbers of base 10 starting from 0.
+*main - Prints all single digit numbers of base 10 starting from 0,
+*only using putchar and without char variables.
 *
-* Return: Always 0 (Success)
+*Return: Always 0.
 */
-
 int main(void)
 {
 int num;
@@ -13,7 +13,7 @@ int num;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
 
-putchar('\0');
+putchar('\n');
 
 return (0);
 }
