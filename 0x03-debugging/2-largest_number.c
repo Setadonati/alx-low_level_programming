0710ab9e561a26c0e46c101c4fe6ec402ded46db
Setadonati <setadonati@gmail.com>
2022-07-07 20:34:03 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,24 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
-* main - causes an infinite loop
-* Return: 0
-*/
-
-int main(void)
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
 {
-  int i;
+	int largest;
 
-  printf("Infinite loop incoming :(\n");
-
-  i = 0;
-  /*
-  * while (i < 10)
-  * {
-  *      putchar(i);
-  *}
-  */
-  printf("Infinite loop avoided! \\o/\n");
-
-  return (0);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
