73478d49d0eb6730fa266a992bcb26f74a76763c
Setadonati <setadonati@gmail.com>
2022-08-03 10:49:02 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Print a name
- * @name:name
- * @f:ponter to function
- * Return: no return
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
